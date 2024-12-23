a99a1a2c3505efa56c69cfa801e604b3a1f3676e
zelim <zelimkhanjachichanov@hotmail.com>
2024-12-23 10:57:42 +0100

    Added dag for creative map

--- DAGs/airflow_analysis.py
+++ DAGs/airflow_analysis.py
@@ -7,6 +7,8 @@ import numpy as np
 from pandas.api.types import is_numeric_dtype
 import seaborn as sns
 import matplotlib.pyplot as plt
+import geopandas as gpd
+import folium
 
 # Function to combine data
 def combine_data():
@@ -97,6 +99,31 @@ def house_distribution():
 
     print(f"Plot saved to {image_path}")
 
+# Function to create an interactive map
+def create_interactive_map():
+    df = pd.read_csv("../scraper/data/base_row_properties.csv")
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat), crs="EPSG:4326")
+
+    m = folium.Map(location=[50.851409, 4.350694], zoom_start=8)
+
+    for _, row in gdf.iterrows():
+        if pd.notna(row['lat']) and pd.notna(row['lon']):
+            folium.Marker(
+                location=[row['lat'], row['lon']],
+                popup=f"Type: {row['subtype_of_property']}<br>Price: €{row['price']}<br>Address: {row['street']}, {row["postal_code"]} {row["locality_name"]}<br><a href={row["link"]}>Link</a>"
+            ).add_to(m)
+
+    images_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..', 'analysis', 'images')
+ 
+    if not os.path.exists(images_folder):
+        os.makedirs(images_folder)
+
+    image_path = os.path.join(images_folder, 'properties_map.html')
+
+    m.save(image_path)
+
+    print(f"Map saved to {image_path}")
+
 # Default arguments for the DAG
 default_args = {
     'owner': 'veena',
@@ -150,6 +177,11 @@ plot_task = PythonOperator(
     dag=dag,
 )
 
+map_task = PythonOperator(
+    task_id = "interactive_map",
+    python_callable = create_interactive_map,
+    dag = dag
+)
+
 # Set task dependencies
 combine_task >> clean_task >> save_task >> plot_task
-
--- analysis/map.py
+++ analysis/map.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import geopandas as gpd
 import folium
+import sys
+import os
 
 def create_interactive_map():
-    df = pd.read_csv("./scraper/data/base_row_properties.csv")
+    df = pd.read_csv("../scraper/data/base_row_properties.csv")
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat), crs="EPSG:4326")
 
     m = folium.Map(location=[50.851409, 4.350694], zoom_start=8)
 
     for _, row in gdf.iterrows():
-        folium.Marker(
-            location=[row['latitude'], row['longitude']],
-            popup=f"Type: {row['subtype_of_property']}<br>Price: €{row['price']}<br>Address: {row['street']}, {row["postal_code"]} {row["locality_name"]}",
-        ).add_to(m)
+        if pd.notna(row['lat']) and pd.notna(row['lon']):
+            folium.Marker(
+                location=[row['lat'], row['lon']],
+                popup=f"Type: {row['subtype_of_property']}<br>Price: €{row['price']}<br>Address: {row['street']}, {row["postal_code"]} {row["locality_name"]}<br><a href={row["link"]}>Link</a>"
+            ).add_to(m)
 
-    m.save("properties_map.html")
+    images_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..', 'analysis', 'images')
+ 
+    if not os.path.exists(images_folder):
+        os.makedirs(images_folder)
+
+    image_path = os.path.join(images_folder, 'properties_map.html')
+
+    m.save(image_path)
+
+    print(f"Map saved to {image_path}")
